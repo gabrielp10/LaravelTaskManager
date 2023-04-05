0d0e8cf44aa20ffbf6d9d97f3b00270f1de86439
Gabriel Santos <45973564+GabrielTSants@users.noreply.github.com>
2023-04-05 03:13:33 -0300

    Create LinuxInstaller.sh

---
+++ LinuxInstaller.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Clone the repository
+git clone https://github.com/GabrielTSants/LaravelTaskManager;
+
+# Enter the repository
+cd LaravelTaskManager;
+
+# Create the .env
+cp .env.example .env;
+
+# Run the docker
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    laravelsail/php81-composer:latest \
+    composer install --ignore-platform-reqs;
+
+# Start the server 
+./vendor/bin/sail up -d;
+
+# Generate the key
+./vendor/bin/sail php artisan key:generate;
+
+# You can now access the project in 0.0.0.0:80 and PHPMyAdmin in 0.0.0.0:8080
